4c41e6a16ea2c442e078d4b40105d8e48c3f8950
Kelly <36lima@gmail.com>
2015-10-31 15:58:40 -0400

    Initial Notes on Paper/Rock/Scissors game.

---
+++ Week1-Exercises/prs.rb
@@ -0,0 +1,12 @@
+# RPS Game Play Notes
+# Play R/R/S!
+# Choose one: (P/R/S)
+# mixed case acceptable
+# Paper wraps rock
+# Scissors cuts paper
+# Rock dulls scissors
+# Determine logic
+# Computer randomly pick value.
+# Tie condition
+# Keep score?
+# end with Play again? (Y/N)
