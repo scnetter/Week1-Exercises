3b1c47e0e6c6d52d652673fa6556d04e5fd0a875
Kelly <36lima@gmail.com>
2015-10-31 15:51:26 -0400

    Added check for divide by zero and loop with exit condition.

--- calculator.rb
+++ calculator.rb
@@ -1,35 +1,45 @@
 def say(msg)
   puts "=> #{msg}"
 end
+operator = ''
+loop do
+  say "What's the first number?"
+  num1 = gets.chomp
 
-say "What's the first number?"
-num1 = gets.chomp
-
-say "What's the second number?"
-num2 = gets.chomp
-
-
-say "1) add 2) subtract 3) multiply 4) divide"
-operator = gets.chomp.to_i
-
-operation = ["add", "subtract", "multiply", "divide"]
+  say "What's the second number?"
+  num2 = gets.chomp
 
 
-puts "You entered Numbers: #{num1} and #{num2} and you chose to #{operation[operator -1]}"
+  say "1) Addition 2) Subtraction 3) Multiplication 4) Division"
+  operator = gets.chomp.to_i
 
-case operator.to_s
+  operation = ["add", "subtract", "multiply", "divide"]
 
-when '1'
-    result = num1.to_i + num2.to_i
-when '2'
-    result = num1.to_i - num2.to_i
-when '3'
-    result = num1.to_i * num2.to_i
-when '4'
-    result = num1.to_i / num2.to_i
-else
-    result = "You entered an invalid operation."
+
+  puts "You entered Numbers: #{num1} and #{num2} and you chose to #{operation[operator - 1]}"
+
+
+  case operator.to_s
+
+  when '1'
+      result = num1.to_i + num2.to_i
+  when '2'
+      result = num1.to_i - num2.to_i
+  when '3'
+      result = num1.to_i * num2.to_i
+  when '4'
+    if num2.to_i == 0
+      result = "You cannot divide by zero!"
+    else
+      result = num1.to_f / num2.to_f
+    end
+  else
+      result = "You entered an invalid operation."
+  end
+
+
+  puts "\n\n The answer is: #{result}." 
+  puts "Do another problem?[\'n\' to exit, anything else to continue]:" 
+  if gets.chomp.downcase == 'n' then break end 
+
 end
-
-
-puts result     
